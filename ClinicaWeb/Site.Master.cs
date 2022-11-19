e14545b176b0dc1d2ca771f881d565e82b5730a6
Norman Ruiz <norman.ruiz@outlook.com.ar>
2022-11-19 19:19:35 -0300

    Agrego funcionalidad verificacion de perfiles en pantallas de administracion.

--- ClinicaWeb/Site.Master.cs
+++ ClinicaWeb/Site.Master.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Modelo;
+
 
 namespace ClinicaWeb
 {
@@ -13,5 +15,63 @@ namespace ClinicaWeb
         {
 
         }
+
+        public bool ValidarRecepcionista()
+        {
+            bool estado = true;
+            Modelo.Usuario usuario;
+            try
+            {
+                usuario = new Modelo.Usuario();
+                usuario = (Modelo.Usuario)Session["usuario"];
+                if (usuario.perfil.Tipo == "Recepcionista")
+                {
+                    estado = true;
+                }
+                else
+                {
+                    estado = false;
+                }
+                return estado;
+            }
+            catch (Exception excepcion)
+            {
+                estado = false;
+                Session.Add("pagOrigen", "Turnos.aspx");
+                Session.Add("excepcion", excepcion);
+                Response.Redirect("Error.aspx", false);
+                return estado;
+            }
+        }
+
+        public bool ValidarAdministrador()
+        {
+            bool estado = true;
+            Modelo.Usuario usuario;
+            try
+            {
+                usuario = new Modelo.Usuario();
+                usuario = (Modelo.Usuario)Session["usuario"];
+                if (usuario.perfil.Tipo == "Administrador")
+                {
+                    estado = true;
+                }
+                else
+                {
+                    estado = false;
+                }
+                return estado;
+            }
+            catch (Exception excepcion)
+            {
+                estado = false;
+                Session.Add("pagOrigen", "Turnos.aspx");
+                Session.Add("excepcion", excepcion);
+                Response.Redirect("Error.aspx", false);
+                return estado;
+            }
+
+        }
+
     }
 }
